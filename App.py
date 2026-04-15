d073df838e9ba043463d90e1eefe783a8f786f91
kkavitah83-dev <kkavitah83@gmail.com>
2026-04-15 12:21:09 +0530

    Update App.py

--- App.py
+++ App.py
@@ -14,9 +14,9 @@ if st.button("Generate Content"):
     model="llama-3.3-70b-versatile",
     messages=[{"role": "user", "content": prompt}]
   )
-st.session_state.text = response.choices[0].message.content
-text = response.choices[0].message.content
-st.write(text)
+  st.session_state.text = response.choices[0].message.content
+  text = response.choices[0].message.content
+  st.write(text)
 #After Content Create - Download The File
 if "text" in st.session_state:
   content = st.text_area("Generated Content",st.session_state.text,height-300)
